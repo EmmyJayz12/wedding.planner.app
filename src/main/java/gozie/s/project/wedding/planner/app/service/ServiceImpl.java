f3c81032cd5b6c52ebd42c900e6812694955e8a7
lawgizzy <lawgizzy@users.noreply.github.com>
2020-04-17 14:36:25 +0100

    Added service implementation class

--- src/main/java/gozie/s/project/wedding/planner/app/service/ServiceImpl.java
+++ src/main/java/gozie/s/project/wedding/planner/app/service/ServiceImpl.java
@@ -2,12 +2,19 @@ package gozie.s.project.wedding.planner.app.service;
 
 import gozie.s.project.wedding.planner.app.model.response.BaseResponse;
 import gozie.s.project.wedding.planner.app.model.PlannerPersonalDetails;
+import gozie.s.project.wedding.planner.app.model.response.PlannerInfo;
+import gozie.s.project.wedding.planner.app.model.response.PlannerInformation;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 @Service
-public class ServiceImpl {
+public class ServiceImpl<Planner> {
     private JdbcTemplate jdbcTemplate;
 
     public ServiceImpl(DataSource dataSource){
@@ -52,5 +59,48 @@ public class ServiceImpl {
             base.setResponseMessage(e.getCause().getMessage());
         }
         return base;
+    }
+    public PlannerInfo getAllPersonalDetails(){
+        BaseResponse base = new BaseResponse();
+        PlannerInfo information = new PlannerInfo();
+        String sql = "SELECT * FROM personal_details";
+        try {
+           List<PlannerPersonalDetails> info= this.jdbcTemplate.query(sql, new RowMapperInfo());
+           if(info.isEmpty()|| info==null){
+               information.setResponseCode("10001");
+               information.setResponseMessage("No Record Found");
+               return information;
+           }
+           information.setResponseCode("00");
+           information.setResponseMessage("Successful");
+           information.setPlannerDetails(info);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            base.setResponseCode("100002");
+            base.setResponseMessage(e.getCause().getMessage());
+        }
+        return information;
+    }
+    private class RowMapperInfo implements RowMapper{
+
+        @Override
+        public Object mapRow(ResultSet rs, int i) throws SQLException {
+
+            PlannerPersonalDetails details = new PlannerPersonalDetails();
+            details.setId(rs.getLong("id"));
+            details.setCompanyName(rs.getString("company_name"));
+            details.setFirstName(rs.getString("first_name"));
+            details.setMiddleName(rs.getString("middle_name"));
+            details.setSurname(rs.getString("surname"));
+            details.setOfficeAddress(rs.getString("office_address"));
+            details.setState(rs.getString("state"));
+            details.setPhoneNumber(rs.getString("phone_number"));
+            details.setEmailAddress(rs.getString("email_address"));
+            details.setUsername(rs.getString("username"));
+            details.setPassword(rs.getString("password"));
+            details.setDate_time(rs.getString("date_time"));
+            return details;
+        }
     }
 }
